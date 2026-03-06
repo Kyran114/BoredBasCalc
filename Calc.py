6181a5742af93a9c12ab5c4679e3c44afe505385
Denis Silva Meira <denissilvameira@outlook.com>
2026-03-06 10:13:54 +0100

    Reworked

--- Calc.py
+++ Calc.py
@@ -1,49 +1,65 @@
-class Calculator():
-    result = 0
-    def add(self):
-        result =var1 + var2
-        return result
-    
-    def sub(self):
-        result =var1 - var2
-        return result
-    
-    def mult(self):
-        result =var1 * var2
-        return result
-    
-    def div(self):
-        result =var1 / var2
-        return result
+import Calculator
 
 run = int(1)
-while run != 0:
-        var1 = int(input("Please enter: Variable 1 = "))
-        var2 = int(input("Please enter: Variable 2 = "))
-        print("1.Add")
-        print("2.Sub")
-        print("3.Mult")
-        print("4.Div")
-        operator = input("Please enter: the desired operator choice = ")
-        cal =Calculator()
+while run > 0:
+    try:
+        cal =Calculator.Calculator()
+        print("-------------------------------")
+        print("Welcome to the Calculator :)")
+        print("-------------------------------")
+        var1 = int(input("Variable 1: "))
+        var2 = int(input("Variable 2: "))
+        print("-------------------------------")
+        print("Please enter: the desired operator choice as a number: ")
+        print("1:Add  2:Sub  3:Mult  4:Div")
+        print("-------------------------------")
+        operator = int(input())
+        print("-------------------------------")
+        print("Calculating...")
         if operator == 1 :
-            print(cal.add())
+            print(f"The result = {cal.add(var1, var2)}")
+            pass
         elif operator == 2:
-            print(cal.sub())
+            print(f"The result = {cal.sub(var1, var2)}")
+            pass
         elif operator == 3:
-            print(cal.mult())
+            print(f"The result = {cal.mult(var1, var2)}")
+            pass
         elif operator == 4:
-            print(cal.div())
+            print(f"The result = {cal.div(var1, var2)}")
+            pass
         else:
-            print("invalid operator")
-        dec = input("Another Calculation ?")
+            print("Invalid Number, try again")
+            continue
+            
+    except Exception as e:
+            print("Error: ", e)
+            continue
+    try:
+        print("-------------------------------")
+        print("Another Calculation ?")
+        print("Please enter your choice as a number: ")
+        print("-------------------------------")
         print("1.Yes")
         print("2.No") 
-        if dec == "Y"or "y":
-            run = 1
-        elif dec == "N" or "n":
-            run = 0
-            exit
+        print("-------------------------------")
+        dec = int(input("Your choice = "))
+        if dec == 1:
+            continue
+        elif dec == 2:
+            print("Good Bye")
+            exit()
+        else:
+            print("Invalid Operator, try again")
+            continue
+
+    except Exception as e:
+        print("Invalid Operator")
+        break
+
+
+                
+        
 
         
 
---
+++ Calculator.py
@@ -0,0 +1,16 @@
+class Calculator():
+    def add(self,var1,var2):
+        result = var1 + var2
+        return result
+    
+    def sub(self,var1,var2):
+        result =var1 - var2
+        return result
+    
+    def mult(self,var1,var2):
+        result =var1 * var2
+        return result
+    
+    def div(self,var1,var2):
+        result =var1 / var2
+        return result
